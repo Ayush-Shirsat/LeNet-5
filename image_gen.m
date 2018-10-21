5fa841709258ef9fa0da5b92db012799ede7b4c9
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-10-20 23:17:05 -0400

    Update image_gen.m

--- image_gen.m
+++ image_gen.m
@@ -2,7 +2,12 @@ clear all
 close all
 clc
 
-img = imread('dollar_train.jpg');
+img1 = imresize(imread('dollar_train.jpg'), [820,680]);
+img2 = imresize(imread('Pound_train.PNG'), [820,680]);
+img3 = imresize(imread('Euro_train.PNG'), [820,680]);
+img4 = imresize(imread('Rupee_train.PNG'), [820,680]);
+img5 = imresize(imread('Yen_train.PNG'), [820,680]);
+img = [img1;img2;img3;img4;img5];
 % imshow(img);
 
 gray = rgb2gray(img);
@@ -53,13 +58,125 @@ for c = 1:len
         crop = imresize(crop,[28,28]);
         mm=num2str(count);
         if count < 10
-            mm1=strcat('00',mm);
+            mm1=strcat('0000',mm);
         elseif count < 100
+            mm1=strcat('000',mm);
+        elseif count < 1000
+            mm1=strcat('00',mm);
+        elseif count < 10000
             mm1=strcat('0',mm);
         else 
             mm1=strcat(mm);
         end
         imwrite(crop,strcat('img',num2str(mm1),'.jpg'));
+        
+        rot5 = imrotate(crop, 5);
+        [r,c]= size(rot5);
+        for i = 1:r
+            for j = 1:c
+               if (rot5(i,j) == 0)  
+                   rot5(i,j) = 255;
+               else
+                   rot5(i,j) = rot5(i,j);
+               end
+            end
+        end
+        count = count + 1;
+        rot5 = imresize(rot5, [28,28]);
+        mm=num2str(count);
+        if count < 10
+            mm1=strcat('0000',mm);
+        elseif count < 100
+            mm1=strcat('000',mm);
+        elseif count < 1000
+            mm1=strcat('00',mm);
+        elseif count < 10000
+            mm1=strcat('0',mm);
+        else 
+            mm1=strcat(mm);
+        end
+        imwrite(rot5,strcat('img',num2str(mm1),'.jpg'));
+        
+        rotm5 = imrotate(crop, -5);
+        [r,c]= size(rotm5);
+        for i = 1:r
+            for j = 1:c
+               if (rotm5(i,j) == 0)  
+                   rotm5(i,j) = 255;
+               else
+                   rotm5(i,j) = rotm5(i,j);
+               end
+            end
+        end
+        count = count + 1;
+        rotm5 = imresize(rotm5, [28,28]);
+        mm=num2str(count);
+        if count < 10
+            mm1=strcat('0000',mm);
+        elseif count < 100
+            mm1=strcat('000',mm);
+        elseif count < 1000
+            mm1=strcat('00',mm);
+        elseif count < 10000
+            mm1=strcat('0',mm);
+        else 
+            mm1=strcat(mm);
+        end
+        imwrite(rotm5,strcat('img',num2str(mm1),'.jpg'));
+        
+        rot10 = imrotate(crop, 10);
+        [r,c]= size(rot10);
+        for i = 1:r
+            for j = 1:c
+               if (rot10(i,j) == 0)  
+                   rot10(i,j) = 255;
+               else
+                   rot10(i,j) = rot10(i,j);
+               end
+            end
+        end
+        count = count + 1;
+        rot10 = imresize(rot10, [28,28]);
+        mm=num2str(count);
+        if count < 10
+            mm1=strcat('0000',mm);
+        elseif count < 100
+            mm1=strcat('000',mm);
+        elseif count < 1000
+            mm1=strcat('00',mm);
+        elseif count < 10000
+            mm1=strcat('0',mm);
+        else 
+            mm1=strcat(mm);
+        end
+        imwrite(rot10,strcat('img',num2str(mm1),'.jpg'));
+        
+        rotm10 = imrotate(crop, 10);
+        [r,c]= size(rotm10);
+        for i = 1:r
+            for j = 1:c
+               if (rotm10(i,j) == 0)  
+                   rotm10(i,j) = 255;
+               else
+                   rotm10(i,j) = rotm10(i,j);
+               end
+            end
+        end
+        count = count + 1;
+        rotm10 = imresize(rotm10, [28,28]);
+        mm=num2str(count);
+        if count < 10
+            mm1=strcat('0000',mm);
+        elseif count < 100
+            mm1=strcat('000',mm);
+        elseif count < 1000
+            mm1=strcat('00',mm);
+        elseif count < 10000
+            mm1=strcat('0',mm);
+        else 
+            mm1=strcat(mm);
+        end
+        imwrite(rotm10,strcat('img',num2str(mm1),'.jpg'));
     end
 end
 disp(count);
